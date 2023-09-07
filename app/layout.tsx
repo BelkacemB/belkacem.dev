69ab442297bf283f4b2bdede78561c075ec4aa56
Belkacem Berchiche <b_berchiche@esi.dz>
2023-09-07 23:12:29 +0200

    fix(front): fix netlify build

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,66 +5,66 @@ import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
-  title: {
-    default: "belkacem.dev",
-    template: "%s | belkacem.dev",
-  },
-  description: "Software engineer at tracklab.co and founder of haul9000.team",
-  openGraph: {
-    title: "belkacem.dev",
-    description:
-      "Software engineer at tracklab.co and founder of haul9000.team",
-    url: "https://belkacem.dev",
-    siteName: "belkacem.dev",
-    locale: "en-US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "Belkacem Berchiche",
-    card: "summary_large_image",
-  },
-  icons: {
-    shortcut: "/favicon.png",
-  },
+	title: {
+		default: "belkacem.dev",
+		template: "%s | belkacem.dev",
+	},
+	description: "Software engineer at tracklab.co and founder of haul9000.team",
+	openGraph: {
+		title: "belkacem.dev",
+		description:
+			"Software engineer at tracklab.co and founder of haul9000.team",
+		url: "https://belkacem.dev",
+		siteName: "belkacem.dev",
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "Belkacem Berchiche",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicon.png",
+	},
 };
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+	subsets: ["latin"],
+	variable: "--font-inter",
 });
 
 const calSans = LocalFont({
-  src: "../public/fonts/CalSans-SemiBold.ttf",
-  variable: "--font-calsans",
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
-      <head>
-        <Analytics />
-      </head>
-      <body
-        className={`bg-black ${
-          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-        }`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
